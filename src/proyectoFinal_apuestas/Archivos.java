fdf184904933429d59e34df806fff30291b0d430
Abel <abeljosue.ma279@gmail.com>
2025-05-17 16:20:54 -0400

    Segunda

---
+++ src/proyectoFinal_apuestas/Archivos.java
@@ -0,0 +1,21 @@
+package proyectoFinal_apuestas;
+
+public interface Archivos {
+	static final String archivosUsuarios = "usuarios.txt";
+	static final String archivoUsuariosBin = "usuarios.dat";
+	static final String archivosAdministradores = "administradores.txt";
+	static final String archivoAdministradoresBin = "administradores.dat";
+	static final String archivosEventosFutbol = "eventosFutbol.txt";
+	static final String archivosEventosFutbolBin = "eventosFutbol.dat";
+	static final String archivosEventosBasketball = "eventosBasketball.txt";
+	static final String archivosEventosBasketballBin = "eventosBasketball.dat";
+	static final String archivosPrediccionesFutbol = "prediccionesFutbol.txt";
+	static final String archivosPrediccionesFutbolBin = "prediccionesFutbol.dat";
+	static final String archivosPrediccionesBasketball = "prediccionesBasketball.txt";
+	static final String archivosPrediccionesBasketballBin = "prediccionesBasketball.dat";
+	static final String archivosApuestasFutbol = "apuestasFutbol.txt";
+	static final String archivosApuestasFutbolBin = "apuestasFutbol.dat";
+	static final String archivosApuestasBasketball = "apuestasBasketball.txt";
+	static final String archivosApuestasBasketballBin = "apuestasBasketball.dat";
+}
+
