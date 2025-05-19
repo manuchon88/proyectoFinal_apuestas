ef7ddf649b839f6db8a0a79dc7211dbe79965573
Abel <abeljosue.ma279@gmail.com>
2025-05-19 03:24:58 -0400

    Tercera

--- src/proyectoFinal_apuestas/ResultadoBasketball.java
+++ src/proyectoFinal_apuestas/ResultadoBasketball.java
@@ -1,5 +1,16 @@
 package proyectoFinal_apuestas;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class ResultadoBasketball {
 	private int puntos1, puntos2, triples1, triples2, faltas1, faltas2;
 
@@ -65,5 +76,99 @@ public class ResultadoBasketball {
 	public String toString() {
 		return puntos1+", "+puntos2+", "+triples1+", "+triples2+", "+faltas1+", "+faltas2;
 	}
+	
+	public boolean registrarResultadosBasketballTxt(String archivo) {
+		try {
+			PrintWriter escritor = new PrintWriter(new FileWriter(archivo,true));
+			String registro = toString();
+			escritor.println(registro);
+			escritor.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return false;
+		}	
+		return true;
+    }
 
+	public static ArrayList<ResultadoBasketball> leerResultadosBasketballTxt(String archivo) {
+		ArrayList<ResultadoBasketball> resultadosBasketball = new ArrayList<>();
+		try (BufferedReader lector = new BufferedReader(new FileReader(archivo))) {
+			String linea;
+			while ((linea = lector.readLine()) != null) {
+				String[] datos = linea.split(", ");
+				int puntos1 = Integer.parseInt(datos[0]);
+				int puntos2 = Integer.parseInt(datos[1]);
+				int triples1 = Integer.parseInt(datos[2]);
+				int triples2 = Integer.parseInt(datos[3]);
+				int faltas1 = Integer.parseInt(datos[4]);
+				int faltas2 = Integer.parseInt(datos[5]);
+				resultadosBasketball.add(new ResultadoBasketball(puntos1, puntos2, triples1, triples2, faltas1, faltas2));
+			}
+		} catch (IOException e) {
+			System.out.println("Error");
+		}
+		return resultadosBasketball;
+	}
+
+	public boolean registrarResultadosBasketballBin(String archivo) {
+		try (DataOutputStream escritor = new DataOutputStream(new FileOutputStream(archivo, true))) {
+			escritor.writeInt(puntos1);
+			escritor.writeInt(puntos2);
+			escritor.writeInt(triples1);
+			escritor.writeInt(triples2);
+			escritor.writeInt(faltas1);
+			escritor.writeInt(faltas2);
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static ArrayList<ResultadoBasketball> leerResultadosBasketballBin(String archivo) {
+	    ArrayList<ResultadoBasketball> resultadosBasketball = new ArrayList<ResultadoBasketball>();
+	    try (DataInputStream lector = new DataInputStream(new FileInputStream(archivo))) {
+	        while (lector.available() > 0) {
+	            int puntos1 = lector.readInt();
+	            int puntos2 = lector.readInt();
+	            int triples1 = lector.readInt();
+	            int triples2 = lector.readInt();
+	            int faltas1 = lector.readInt();
+	            int faltas2 = lector.readInt();
+	            resultadosBasketball.add(new ResultadoBasketball(puntos1, puntos2, triples1, triples2, faltas1, faltas2));
+	        }
+	    } catch (IOException e) {
+	        System.out.println("Error");
+	    }
+	    return resultadosBasketball;
+	}
+
+
+	public static boolean reescribirResultadosBasketballTxt(ArrayList<ResultadoBasketball> resultadosBasketball, String archivo) {
+		try (PrintWriter escritor = new PrintWriter(new FileWriter(archivo))) {
+			for (ResultadoBasketball r : resultadosBasketball) {
+				String registro = r.getPuntos1() + ", " + r.getPuntos2() + ", " + r.getTriples1() +
+						", " + r.getTriples2() + ", " + r.getFaltas1() + ", " + r.getFaltas2();
+				escritor.println(registro);
+			}
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean reescribirResultadosBasketballBin(ArrayList<ResultadoBasketball> resultados, String archivo) {
+		try (DataOutputStream escritor = new DataOutputStream(new FileOutputStream(archivo))) {
+			for (ResultadoBasketball r : resultados) {
+				escritor.writeInt(r.getPuntos1());
+				escritor.writeInt(r.getPuntos2());
+				escritor.writeInt(r.getTriples1());
+				escritor.writeInt(r.getTriples2());
+				escritor.writeInt(r.getFaltas1());
+				escritor.writeInt(r.getFaltas2());
+			}
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
 }
--- src/proyectoFinal_apuestas/ResultadoFutbol.java
+++ src/proyectoFinal_apuestas/ResultadoFutbol.java
@@ -1,5 +1,17 @@
 package proyectoFinal_apuestas;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class ResultadoFutbol {
     private int goles1, goles2, amarillas1, amarillas2, rojas1, rojas2;
 
@@ -65,5 +77,84 @@ public class ResultadoFutbol {
 		this.rojas1 = rojas1;
 		this.rojas2 = rojas2;
 	}
+	
+	public boolean registrarResultadosFutbolTxt(String archivo) {
+		try {
+			PrintWriter escritor = new PrintWriter(new FileWriter(archivo,true));
+			String registro = toString();
+			escritor.println(registro);
+			escritor.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return false;
+		}	
+		return true;
+    }
 
+    public static ArrayList<ResultadoFutbol> leerResultadosFutbolTxt(String archivo) {
+        ArrayList<ResultadoFutbol> resultadosFutbol = new ArrayList<ResultadoFutbol>();
+        try (BufferedReader lector = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = lector.readLine())!=null) {
+                String[] datos = linea.split(", ");
+                int goles1 = Integer.parseInt(datos[0]);
+                int goles2 = Integer.parseInt(datos[1]);
+                int amarillas1 = Integer.parseInt(datos[2]);
+                int amarillas2 = Integer.parseInt(datos[3]);
+                int rojas1 = Integer.parseInt(datos[4]);
+                int rojas2 = Integer.parseInt(datos[5]);
+                resultadosFutbol.add(new ResultadoFutbol(goles1, goles2, amarillas1, amarillas2, rojas1, rojas2));
+            }
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        return resultadosFutbol;
+    }
+
+    public boolean registrarResultadosFutbolBin(String archivo) {
+        try {
+        	DataOutputStream escritor = new DataOutputStream(new FileOutputStream(archivo, true));
+            escritor.writeInt(goles1);
+            escritor.writeInt(goles2);
+            escritor.writeInt(amarillas1);
+            escritor.writeInt(amarillas2);
+            escritor.writeInt(rojas1);
+            escritor.writeInt(rojas2);
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean reescribirResultadosFutbolTxt(ArrayList<ResultadoFutbol> resultados, String archivo) {
+        try (PrintWriter escritor = new PrintWriter(new FileWriter(archivo))) {
+            for (ResultadoFutbol r : resultados) {
+                String registro = r.getGoles1() + ", " + r.getGoles2() + ", " + r.getAmarillas1() + ", " +
+                		r.getAmarillas2() + ", " + r.getRojas1() + ", " + r.getRojas2();
+                escritor.println(registro);
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static boolean reescribirResultadosFutbolBin(ArrayList<ResultadoFutbol> resultadosFutbol, String archivo) {
+        try (DataOutputStream escritor = new DataOutputStream(new FileOutputStream(archivo))) {
+            for (ResultadoFutbol r : resultadosFutbol) {
+                escritor.writeInt(r.getGoles1());
+                escritor.writeInt(r.getGoles2());
+                escritor.writeInt(r.getAmarillas1());
+                escritor.writeInt(r.getAmarillas2());
+                escritor.writeInt(r.getRojas1());
+                escritor.writeInt(r.getRojas2());
+            }
+			escritor.close();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+        
 }
